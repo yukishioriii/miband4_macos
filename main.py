045517ed7182bcc175caaa4b89a3a06cfc2d43cf
AN DO <anmana000@gmail.com>
2023-05-23 05:10:03 +0700

    wip getting activity

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ import struct
 from bleak import BleakScanner, BleakClient
 from constants import AUTH_STATES, UUIDS
 from Crypto.Cipher import AES
+from datetime import datetime, timedelta
 
 RANDOM_BYTE = struct.pack('<2s', b'\x02\x00')
-
+DEFAULT_TIMEOUT = 0.5
 with open("secret.txt", "r") as f:
     MAC_ADDRESS, AUTH_KEY = f.read().split("\n")
 
@@ -23,6 +24,18 @@ class Characteristic:
 
     async def read(self):
         return await self.client.read_gatt_char(self.char_specifier)
+
+
+class Descriptor:
+    def __init__(self, char_specifier: str, client: BleakClient) -> None:
+        self.char_specifier = char_specifier
+        self.client = client
+
+    async def write(self, value):
+        await self.client.write_gatt_descriptor(self.char_specifier, value)
+
+    async def read(self):
+        return await self.client.read_gatt_descriptor(self.char_specifier)
 
 
 class Wac:
@@ -63,6 +76,79 @@ class StepChar(Characteristic):
             "fat_burned": fat_burned,
             "calories": calories
         }
+
+
+class ActivityGetter:
+    def __init__(self) -> None:
+        self.fetch_char = FetchChar()
+        self.
+
+
+class FetchChar(Characteristic):
+    def __init__(self, start, end, utc_offset, pkg, char_specifier: str, client: BleakClient):
+        super().__init__(char_specifier, client)
+        self.desc = Descriptor(char_specifier, client)
+        self.listening = False
+        self.time_offset = utc_offset
+        self.start_timestamp = start
+        self.end_timestamp = end
+        self.getting_timestamp = start
+        self.pkg = pkg
+
+    async def set_descriptor(self, value: True | False):
+        if value:
+            await self.desc.write(b"\x01\x00")
+        else:
+            await self.desc.write(b"\x00\x00")
+
+    async def send_fetch_payload(self):
+        if not self.listening:
+            await self.set_descriptor(True)
+            await asyncio.sleep(0.1)
+        ts = self._pack_timestamp(self.start_timestamp)
+        payload = b'\x01\x01' + ts + self.utc_offset
+        self.write(payload)
+        self.listening = True
+
+    def _pack_timestamp(self, timestamp: datetime):
+        year = struct.pack("<H", timestamp.year)
+        month = struct.pack("b", timestamp.month)
+        day = struct.pack("b", timestamp.day)
+        hour = struct.pack("b", timestamp.hour)
+        minute = struct.pack("b", timestamp.minute)
+        ts = year + month + day + hour + minute
+        return ts
+
+    async def callback(self, char_specifier, data):
+        if data[:3] == b'\x10\x01\x01':
+            year = struct.unpack("<H", data[7:9])[0]
+            month = struct.unpack("b", data[9:10])[0]
+            day = struct.unpack("b", data[10:11])[0]
+            hour = struct.unpack("b", data[11:12])[0]
+            minute = struct.unpack("b", data[12:13])[0]
+            self.getting_timestamp = datetime(year, month, day, hour, minute)
+            print(f"Fetch data from {self.getting_timestamp}")
+            self.pkg = 0  # reset the packing index
+            self.write(b'\x02')
+        elif data[:3] == b'\x10\x02\x01':
+            if self.activity_getter.last_timestamp > self.activity_getter.end_timestamp - timedelta(minutes=1):
+                print("Finished fetching")
+                
+                return
+            await asyncio.sleep(1)
+            t = self.device.last_timestamp + timedelta(minutes=1)
+            print(f"Trigger more communication {t}")
+            self.send_fetch_payload(t)
+
+        elif data[:3] == b'\x10\x02\x04':
+            print("No more activity fetch possible")
+            return
+        else:
+            print("Unexpected data on handle " + str(hnd) + ": " + str(data))
+            return
+
+    async def init_handler(self):
+        await self.client.start_notify(self.char_specifier, self.callback)
 
 
 class AuthenticateChar(Characteristic):
